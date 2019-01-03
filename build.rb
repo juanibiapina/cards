193dc9d0ce380968179a79b79321288b6c34b427
Juan Ibiapina <juanibiapina@gmail.com>
2019-01-03 22:47:38 +0100

    Initial card design matching prototype

--- build.rb
+++ build.rb
@@ -3,9 +3,17 @@ require "bundler"
 
 Bundler.require
 
-Squib::Deck.new width: 825, height: 1125, cards: 2 do
-  background color: 'pink'
-  rect
-  text str: ['Hello', 'World!']
-  save_png prefix: 'hello_'
+data = Squib.csv file: "input/data.csv"
+
+Squib::Deck.new cards: data["type"].size, layout: "layout.yml", width: 825, height: 1125, dpi: 300 do
+  background color: "white"
+
+  rect layout: "cut" # cut line as defined by TheGameCrafter
+  rect layout: "safe" # safe zone as defined by TheGameCrafter
+
+  text str: data["type"].map {|i| i.capitalize }, layout: "type"
+  text str: data["power"], layout: "type_right"
+  text str: data["description"], layout: "description"
+
+  save_png
 end
