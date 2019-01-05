e45c9cb6dc631f70668f7f485132375275b116a8
Juan Ibiapina <juanibiapina@gmail.com>
2019-01-05 12:20:24 +0100

    Improve outputs
    - Build a sheet for printing
    - Trim cards for uploading

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
-prototype-2:
-	bundle exec ruby src/prototype-2.rb
+all: prototype-2-pnp prototype-2-cards
+
+prototype-2-pnp:
+	SQUIB_BUILD=print_and_play bundle exec ruby src/prototype-2.rb
+
+prototype-2-cards:
+	SQUIB_BUILD=cards bundle exec ruby src/prototype-2.rb
--- src/prototype-2.rb
+++ src/prototype-2.rb
@@ -11,9 +11,20 @@ Squib::Deck.new cards: data["type"].size, layout: "layouts/layout.yml", width: 8
   rect layout: "cut" # cut line as defined by TheGameCrafter
   rect layout: "safe" # safe zone as defined by TheGameCrafter
 
-  text str: data["type"].map {|i| i.capitalize }, layout: "type"
+  text str: data["type"].map(&:capitalize), layout: "type"
   text str: data["power"], layout: "type_right"
   text str: data["description"], layout: "description"
 
-  save_png
+  build :cards do
+    save_png trim: "0.125in",
+             dir: "output/cards"
+  end
+
+  build :print_and_play do
+    save_sheet prefix: "pnp_sheet_",
+               trim: "0.125in",
+               rows: 3,
+               columns: 3,
+               dir: "output/sheets"
+  end
 end
