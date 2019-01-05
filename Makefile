1b314f3e1d342c98c5eec0b08f105936449dbaa0
Juan Ibiapina <juanibiapina@gmail.com>
2019-01-05 12:40:35 +0100

    Add a proof build
    Also remove safe and cut zones from other builds.

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-all: prototype-2-pnp prototype-2-cards
+all: prototype-2-proof prototype-2-pnp prototype-2-cards
+
+prototype-2-proof:
+	SQUIB_BUILD=proof bundle exec ruby src/prototype-2.rb
 
 prototype-2-pnp:
 	SQUIB_BUILD=print_and_play bundle exec ruby src/prototype-2.rb
--- src/prototype-2.rb
+++ src/prototype-2.rb
@@ -8,12 +8,18 @@ data = Squib.csv file: "data/prototype-2.csv"
 Squib::Deck.new cards: data["type"].size, layout: "layouts/layout.yml", width: 825, height: 1125, dpi: 300 do
   background color: "white"
 
-  rect layout: "cut" # cut line as defined by TheGameCrafter
-  rect layout: "safe" # safe zone as defined by TheGameCrafter
-
   text str: data["type"].map(&:capitalize), layout: "type"
   text str: data["power"], layout: "type_right"
   text str: data["description"], layout: "description"
+
+  build :proof do
+    safe_zone
+    cut_zone
+
+    save_png dir: "output",
+             range: 0,
+             prefix: "proof_"
+  end
 
   build :cards do
     save_png trim: "0.125in",
