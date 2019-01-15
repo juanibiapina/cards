b92e7c10e4ca3a7181b29c36ea1f37b4cc590348
Juan Ibiapina <juanibiapina@gmail.com>
2019-01-15 21:18:44 +0100

    Start creating new layout

--- src/prototype.rb
+++ src/prototype.rb
@@ -9,8 +9,7 @@ Squib::Deck.new cards: data["type"].size, layout: "layouts/layout.yml", width: 8
   background color: "white"
   hint text: :cyan if ENV["SQUIB_BUILD"] == "proof"
 
-  text str: data["type"].map(&:capitalize), layout: "type"
-  text str: data["power"], layout: "type_right"
+  text str: data["name"], layout: "name"
   text str: data["description"], layout: "description"
 
   build :proof do
