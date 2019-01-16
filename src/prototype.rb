1cc753dcd8c5a0527d86fec7600885e56f7deb1e
Juan Ibiapina <juanibiapina@gmail.com>
2019-01-16 22:30:49 +0100

    Work a bit on the layout

--- src/prototype.rb
+++ src/prototype.rb
@@ -5,13 +5,105 @@ Bundler.require
 
 data = Squib.csv file: "data/prototype.csv"
 
-Squib::Deck.new cards: data["type"].size, layout: "layouts/layout.yml", width: 825, height: 1125, dpi: 300 do
+TOTAL_WIDTH = 825
+TOTAL_HEIGHT = 1125
+MARGIN = 75
+CARD_WIDTH = TOTAL_WIDTH - (2 * MARGIN)
+CARD_HEIGHT = TOTAL_HEIGHT - (2 * MARGIN)
+
+SPACE = 10
+LARGE_SPACE = 15
+
+POWER_RADIUS = 50
+POWERS_WIDTH = (2 * POWER_RADIUS)
+NAME_HEIGHT = 50
+ART_HEIGHT = 520
+DESCRIPTION_HEIGHT = 275
+
+Squib::Deck.new cards: data["type"].size, width: TOTAL_WIDTH, height: TOTAL_HEIGHT, dpi: 300 do
   background color: "white"
   hint text: :cyan if ENV["SQUIB_BUILD"] == "proof"
 
-  text str: data["name"], layout: "name"
-  text str: data["description"], layout: "description"
-  png file: data["name"].map { |name| "tmp/#{name}.png" }, layout: "art"
+  # military power
+  circle(
+    x: MARGIN + POWER_RADIUS,
+    y: MARGIN + POWER_RADIUS,
+    radius: POWER_RADIUS,
+  )
+
+  # diplomatic power
+  circle(
+    x: MARGIN + POWER_RADIUS,
+    y: MARGIN + (3 * POWER_RADIUS) + SPACE,
+    radius: POWER_RADIUS,
+  )
+
+  # trade power
+  circle(
+    x: MARGIN + POWER_RADIUS,
+    y: MARGIN + (5 * POWER_RADIUS) + (2 * SPACE),
+    radius: POWER_RADIUS,
+  )
+
+  # name
+  text(
+    str: data["name"],
+    x: MARGIN + POWERS_WIDTH + SPACE,
+    y: MARGIN + SPACE,
+    width: CARD_WIDTH - POWERS_WIDTH - SPACE - SPACE,
+    height: NAME_HEIGHT,
+    font_size: 50,
+  )
+
+  # art
+  png(
+    file: data["name"].map { |name| "tmp/#{name}.png" },
+    x: MARGIN + POWERS_WIDTH + SPACE,
+    y: MARGIN + SPACE + NAME_HEIGHT + SPACE,
+    width: CARD_WIDTH - POWERS_WIDTH - SPACE - SPACE,
+    height: ART_HEIGHT,
+  )
+
+  # description
+  text(
+    str: data["description"],
+    x: MARGIN,
+    y: MARGIN + SPACE + NAME_HEIGHT + SPACE + ART_HEIGHT + SPACE,
+    width: CARD_WIDTH,
+    height: DESCRIPTION_HEIGHT,
+    valign: "middle",
+    align: "center",
+    font_size: 50,
+  )
+
+  # trade cost
+  circle(
+    x: MARGIN + CARD_WIDTH - POWER_RADIUS,
+    y: MARGIN + CARD_HEIGHT - POWER_RADIUS,
+    radius: POWER_RADIUS,
+  )
+
+  # diplomatic cost
+  circle(
+    x: MARGIN + CARD_WIDTH - (3 * POWER_RADIUS) - SPACE,
+    y: MARGIN + CARD_HEIGHT - POWER_RADIUS,
+    radius: POWER_RADIUS,
+  )
+
+  # military cost
+  circle(
+    x: MARGIN + CARD_WIDTH - (5 * POWER_RADIUS) - (2 * SPACE),
+    y: MARGIN + CARD_HEIGHT - POWER_RADIUS,
+    radius: POWER_RADIUS,
+  )
+
+  # victory points
+  rect(
+    x: MARGIN - 10,
+    y: MARGIN + SPACE + NAME_HEIGHT + SPACE + ART_HEIGHT + SPACE + DESCRIPTION_HEIGHT - 10,
+    width: 100 + (2 * 10),
+    height: 100 + (2 * 10),
+  )
 
   build :proof do
     safe_zone
